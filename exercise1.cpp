0074fa9bf8c1ca5a1de0db74fd8ba76fb762ee77
Kozachok_Maria <143996292+humyoo@users.noreply.github.com>
2023-11-22 01:40:46 +0200

    Add files via upload

---
+++ exercise1.cpp
@@ -0,0 +1,58 @@
+﻿// exercise1.cpp 
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+struct Date {
+    int hours;
+    int minutes;
+
+    Date() : hours(0), minutes(0) {}
+
+    Date(int hours, int minutes) : hours(hours), minutes(minutes) {}
+};
+
+int main() {
+
+    Date date;
+
+
+    cout << "Введіть значення годин: ";
+    cin >> date.hours;
+    cout << "Введіть значення хвилин: ";
+    cin >> date.minutes;
+
+    int minutesPassed = 0;
+    cout << "Введіть кількість хвилин, які пройшли: ";
+    cin >> minutesPassed;
+
+    if (date.hours < 0 || date.hours > 24) {
+        cout << "Невірне значення годин!" << endl;
+        return 1;
+    }
+
+    if (date.minutes < 0 || date.minutes > 59) {
+        cout << "Невірне значення хвилин!" << endl;
+        return 1;
+    }
+
+    if (minutesPassed < 0) {
+        cout << "Невірне значення кількості хвилин!" << endl;
+        return 1;
+    }
+
+    date.minutes += minutesPassed;
+    while (date.minutes >= 60) {
+        date.minutes -= 60;
+        date.hours++;
+        if (date.hours >= 24) {
+            date.hours = 0;
+        }
+    }
+
+    cout << "Новий час: " << setfill('0') << setw(2) << date.hours << ":" << setw(2) << date.minutes << endl;
+
+    return 0;
+}
---
+++ exercise2.cpp
@@ -0,0 +1,55 @@
+﻿// exercise2.cpp 
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+struct Date {
+    int hours;
+    int minutes;
+
+    Date() : hours(0), minutes(0) {}
+
+    Date(int hours, int minutes) : hours(hours), minutes(minutes) {}
+};
+
+int main() {
+    Date start, end;
+
+    cout << "Введіть час початку події (години та хвилини): ";
+    cin >> start.hours >> start.minutes;
+    cout << "Введіть час завершення події (години та хвилини): ";
+    cin >> end.hours >> end.minutes;
+
+    if (start.hours < 0 || start.hours > 24) {
+        cout << "Невірне значення годин початку!" << endl;
+        return 1;
+    }
+
+    if (start.minutes < 0 || start.minutes > 59) {
+        cout << "Невірне значення хвилин початку!" << endl;
+        return 1;
+    }
+
+    if (end.hours < 0 || end.hours > 24) {
+        cout << "Невірне значення годин завершення!" << endl;
+        return 1;
+    }
+
+    if (end.minutes < 0 || end.minutes > 59) {
+        cout << "Невірне значення хвилин завершення!" << endl;
+        return 1;
+    }
+
+    int hoursPassed = end.hours - start.hours;
+    int minutesPassed = end.minutes - start.minutes;
+    while (minutesPassed < 0) {
+        minutesPassed += 60;
+        hoursPassed--;
+    }
+
+    cout << "Тривалість події: " << setfill('0') << setw(2) << hoursPassed << ":" << setw(2) << minutesPassed << endl;
+
+    return 0;
+}
